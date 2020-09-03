c195302f765cc5db41f3e66841dfab52f7d96ddb
DmJSCourse <dimonua@gmail.com>
2020-09-03 14:51:14 +0300

    Updates

---
+++ precache-manifest.6b848c70fe046f4af7f164c8f40c4fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6796724a1c69fa308977adf4ccdc37",
+    "url": "/react_goods-selector/index.html"
+  },
+  {
+    "revision": "eb03f721b495d610a42b",
+    "url": "/react_goods-selector/static/css/main.1e914b55.chunk.css"
+  },
+  {
+    "revision": "4dd0f18127de32a9553a",
+    "url": "/react_goods-selector/static/js/2.22b5d565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_goods-selector/static/js/2.22b5d565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb03f721b495d610a42b",
+    "url": "/react_goods-selector/static/js/main.797198a2.chunk.js"
+  },
+  {
+    "revision": "23ceaac2f58521502872",
+    "url": "/react_goods-selector/static/js/runtime-main.df240bf3.js"
+  }
+]);
